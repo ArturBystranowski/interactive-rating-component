3a2a5fad9409c80d3570fbed5e3fc3766f03100e
ArturBystranowski <artur.bystranowski@gmail.com>
2022-09-01 00:00:48 +0200

    Final Version

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("test");
+const rates = document.querySelectorAll(".rate");
+const submitBtn = document.querySelector("button");
+const intro = document.querySelector(".intro");
+const modal = document.querySelector(".modal");
+const choice = document.querySelector(".rate-choice");
+
+const highlightActive = (e) => {
+	rates.forEach((rate) => rate.classList.remove("active"));
+	e.target.classList.add("active");
+};
+
+const proceed = () => {
+	intro.style.display = "none";
+	modal.style.display = "flex";
+	choice.innerHTML = document.querySelector(".active").innerHTML;
+};
+
+rates.forEach((rate) => {
+	rate.addEventListener("click", highlightActive);
+});
+
+submitBtn.addEventListener("click", proceed);
